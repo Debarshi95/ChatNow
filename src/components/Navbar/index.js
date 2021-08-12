a2b2b394d7368fbc45c495710b3f3fb0bf273414
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-08-12 13:02:24 +0530

    Created SignUp & updated Navbar comp

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { memo, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logOut } from '../../store/slices/auth';
+import Modal from '../Modal';
 
 const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
+
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
-    <nav className="w-full border-b-2 border-blue-500">
-      <div className="flex py-3 px-4 justify-between">
-        <div className="flex justify-center items-center font-medium text-blue-500">
+    <nav className="w-full border-b-2">
+      <div className="flex py-2 px-4 justify-between md:w-3/4 mx-auto">
+        <div className="flex justify-center items-center font-semibold text-blue-500 text-lg md:text-xl">
           <Link to="/">ChatNow</Link>
         </div>
-        <div className="rounded-full w-7 h-7 bg-blue-500 flex justify-center items-center text-white font-medium">
-          <Link to="/signin">S</Link>
-        </div>
+        {isAuthenticated ? (
+          <div className="flex">
+            <button
+              type="button"
+              className="bg-blue-500 flex justify-center items-center text-white font-medium
+            px-3 py-2 rounded-sm mr-4 text-sm"
+              onClick={handleModal}
+            >
+              Add User
+            </button>
+            <button
+              type="button"
+              className="bg-blue-500 flex justify-center items-center text-white font-medium
+              px-4 py-2 rounded-sm text-sm"
+              onClick={handleLogout}
+            >
+              Signout
+            </button>
+          </div>
+        ) : (
+          <div className="bg-blue-500 flex justify-center items-center text-white font-medium px-5 py-2 rounded-sm">
+            <Link to="/signup">SignUp</Link>
+          </div>
+        )}
+
+        {isModalOpen && <Modal open={isModalOpen} onRequestClose={handleModal} />}
       </div>
     </nav>
   );
 };
-export default Navbar;
+export default memo(Navbar);
---
+++ src/containers/SignUp/index.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { signUp } from '../../store/slices/auth';
+import FormInput from '../../components/FormInput';
+import routes from '../../utils/routes';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({ username: '', email: '', password: '' });
+  const dispatch = useDispatch();
+  console.log({ formData });
+  const handleForm = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignUp = async () => {
+    const { username, email, password } = formData;
+    if (username === '' || email === '' || password === '') return;
+    try {
+      const res = await dispatch(signUp({ username, email, password })).unwrap();
+      console.log({ res });
+    } catch (error) {
+      //
+    }
+  };
+  return (
+    <div className="w-full md:w-3/5 mx-auto flex justify-center items-center flex-1 h-screen max-h-100vh">
+      <div className="w-full max-w-sm">
+        <form className="px-4 md:px-8 pt-6 pb-8 mb-4" autoComplete="off">
+          <h4 className="text-center font-medium text-blue-500 text-lg mt-4 mb-6">
+            Sign up with ChatNow to get started
+          </h4>
+          <div className="mb-2">
+            <FormInput
+              label="Username"
+              placeholder="Username"
+              type="text"
+              onChange={handleForm}
+              name="username"
+            />
+          </div>
+          <div className="mb-2">
+            <FormInput
+              label="Email"
+              placeholder="johndoe@email.com"
+              type="email"
+              onChange={handleForm}
+              name="email"
+            />
+          </div>
+          <div className="mb-8">
+            <FormInput
+              label="Password"
+              placeholder="******************"
+              type="password"
+              onChange={handleForm}
+              name="password"
+            />
+          </div>
+          <div className="flex items-center justify-center ">
+            <button
+              className="bg-blue-500 flex-1 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={handleSignUp}
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+        <p className="text-center text-blue-500 text-base">
+          Registered?{' '}
+          <Link to={routes.home.route} className="font-semibold">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
